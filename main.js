8dc320822d6fd861ebd541942b208a4c49784981
Max Lyth <maxlyth@users.noreply.github.com>
2021-04-02 00:03:54 +0100

    Add Electron packages for standalone target

---
+++ main.js
@@ -0,0 +1,61 @@
+const { app, BrowserWindow } = require("electron");
+
+const server = require("./app");
+
+var port = normalizePort(process.env.PORT || '3000');
+
+let mainWindow;
+
+function createWindow() {
+  express();
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:" + port + "/");
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("resize", function (e, x, y) {
+  mainWindow.setSize(x, y);
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
